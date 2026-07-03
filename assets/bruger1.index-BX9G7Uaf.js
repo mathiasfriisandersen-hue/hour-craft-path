28c45d99d8056b53215b8de6655b5d6a699e3795
mathiasfriisandersen-hue <mathiasfriisandersen@gmail.com>
2026-07-03 15:13:02 +0200

    Updates

---
+++ assets/bruger1.index-BX9G7Uaf.js
@@ -0,0 +1,1 @@
+import{xt as e}from"./sub-z-logo-BQ3whzNN.js";import{t}from"./app-shell-BQdqmctF.js";import{o as n}from"./index-BszzRSD1.js";var r=e();function i(){return(0,r.jsx)(t,{allow:[`bruger`],children:(0,r.jsx)(n,{role:`bruger`})})}export{i as component};
---
+++ assets/bruger1.workers-txwQnDK1.js
@@ -0,0 +1,1 @@
+import{xt as e}from"./sub-z-logo-BQ3whzNN.js";import{t}from"./app-shell-BQdqmctF.js";import{r as n}from"./index-BszzRSD1.js";var r=e();function i(){return(0,r.jsx)(t,{allow:[`bruger`],children:(0,r.jsx)(n,{role:`bruger`,showBackLink:!0,backHref:`/bruger1`})})}export{i as component};
